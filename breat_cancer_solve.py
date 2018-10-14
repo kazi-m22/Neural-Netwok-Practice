d38894227d15a680c657ba8e6df3f7dbcdd4c736
misbah <222mzb@gmail.com>
2018-10-14 23:45:11 +0600

    breast cancer solve

--- breat_cancer_solve.py
+++ breat_cancer_solve.py
@@ -1,37 +1,41 @@
 import numpy as np
-import pandas as pd
-import keras
-from keras.models import Sequential
-from keras.layers import Dense
 import matplotlib.pyplot as plt
-from sklearn.preprocessing import LabelEncoder, OneHotEncoder
-from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import StandardScaler
-from sklearn.metrics import confusion_matrix
-
+import pandas as pd
 
 dataset = pd.read_csv('data.csv')
-X = dataset.iloc[:,2:].values
-y = dataset.iloc[:,1].values
 
-encoder = LabelEncoder()
-y = encoder.fit_transform(y)
+X = dataset.iloc[:,2:32] # [all rows, col from index 2 to the last one excluding 'Unnamed: 32']
+y = dataset.iloc[:,1]
+
+from sklearn.preprocessing import LabelEncoder
+
+print("Before encoding: ")
+print(y[100:110])
+
+labelencoder_Y = LabelEncoder()
+y = labelencoder_Y.fit_transform(y)
+
+from sklearn.model_selection import train_test_split
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
 
 
-X_train, X_test,y_train,y_test = train_test_split(X,y,test_size=.2)
+from sklearn.preprocessing import StandardScaler
+
+sc = StandardScaler()
+X_train = sc.fit_transform(X_train)
+X_test = sc.transform(X_test)
 
 
+from keras.models import Sequential
+from keras.layers import Dense
 
+classifier = Sequential() # Initialising the ANN
 
-print(X_train.shape)
+classifier.add(Dense(units = 16, kernel_initializer = 'uniform', activation = 'relu', input_dim = 30))
+classifier.add(Dense(units = 8, kernel_initializer = 'uniform', activation = 'relu'))
+classifier.add(Dense(units = 6, kernel_initializer = 'uniform', activation = 'relu'))
+classifier.add(Dense(units = 1, kernel_initializer = 'uniform', activation = 'sigmoid'))
 
-model = Sequential()
-model.add(Dense(output_dim = 64, input_dim=31, activation='relu'))
-model.add(Dense(output_dim = 64, activation='relu'))
-model.add(Dense(output_dim = 64, activation='relu'))
-model.add(Dense(output_dim = 64, activation='relu'))
-
-model.add(Dense(output_dim = 1, activation='sigmoid'))
-
-model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
-model.fit(X_train,y_train, validation_data=(X_test, y_test), batch_size=10, epochs=20)
+classifier.compile(optimizer = 'rmsprop', loss = 'binary_crossentropy', metrics = ['accuracy'])
+classifier.fit(X_train, y_train, batch_size = 1, epochs = 100)
